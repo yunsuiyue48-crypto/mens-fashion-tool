2e5c21675b904fa5e3d614196b9cdaf433a69e09
yunsuiyue48-crypto <yunsuiyue48@gmail.com>
2026-08-14 16:20:08 +0800

    Update open-direction-data.js

--- data/open-direction-data.js
+++ data/open-direction-data.js
@@ -9,7 +9,7 @@ const OPEN_DIRECTION_DATA = {
         "夏季"
       ],
       "image": "assets/styles/sets/图片1.png",
-      "pdf": ""
+      "pdf": "assets/styles/sets/summer  set.pdf"
     },
     {
       "name": "短袖衬衫+短裤套装",
